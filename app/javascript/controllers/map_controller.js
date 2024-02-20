179c041d27e0c084e637016bd725908099df62cc
Raecine Green <raecine_g@hotmail.com>
2024-02-20 22:02:47 +0900

    changed style of map to dark and tried to add heatmap

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -14,11 +14,12 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
+      style: "mapbox://styles/mapbox/dark-v11"
     })
 
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
+    this.#addHeatmapLayer()
 
     this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken, mapboxgl: mapboxgl }))
   }
@@ -38,4 +39,149 @@ export default class extends Controller {
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
   }
+
+  #addHeatmapLayer() {
+
+    this.map.addLayer({
+      id: 'heatmap-layer',
+      type: 'heatmap',
+      source: {
+        type: 'geojson',
+        data: 'trees.geojson',
+      },
+      paint: {
+        'heatmap-weight': {
+          property: 'dbh',
+          type: 'exponential',
+          stops: [
+            [1, 0],
+            [62, 1],
+          ],
+        },
+        'heatmap-intensity': 1.5,
+        'heatmap-color': [
+          'interpolate',
+          ['linear'],
+          ['heatmap-density'],
+          0, 'rgba(0,0,255,0)',
+          0.1, 'royalblue',
+          0.3, 'cyan',
+          0.5, 'lime',
+          0.7, 'yellow',
+          1, 'red',
+        ],
+        'heatmap-radius': 25,
+      },
+    });
+  }
+
+  // map.on('load', () => {
+  //   map.addSource('trees', {
+  //     type: 'geojson',
+  //     data: 'app/javascript/trees.geojson'
+  //   });
+
+  //   map.addLayer(
+  //     {
+  //       id: 'trees-heat',
+  //       type: 'heatmap',
+  //       source: 'trees',
+  //       maxzoom: 15,
+  //       paint: {
+  //         // increase weight as diameter breast height increases
+  //         'heatmap-weight': {
+  //           property: 'dbh',
+  //           type: 'exponential',
+  //           stops: [
+  //             [1, 0],
+  //             [62, 1]
+  //           ]
+  //         },
+  //         // increase intensity as zoom level increases
+  //         'heatmap-intensity': {
+  //           stops: [
+  //             [11, 1],
+  //             [15, 3]
+  //           ]
+  //         },
+  //         // assign color values be applied to points depending on their density
+  //         'heatmap-color': [
+  //           'interpolate',
+  //           ['linear'],
+  //           ['heatmap-density'],
+  //           0,
+  //           'rgba(236,222,239,0)',
+  //           0.2,
+  //           'rgb(208,209,230)',
+  //           0.4,
+  //           'rgb(166,189,219)',
+  //           0.6,
+  //           'rgb(103,169,207)',
+  //           0.8,
+  //           'rgb(28,144,153)'
+  //         ],
+  //         // increase radius as zoom increases
+  //         'heatmap-radius': {
+  //           stops: [
+  //             [11, 15],
+  //             [15, 20]
+  //           ]
+  //         },
+  //         // decrease opacity to transition into the circle layer
+  //         'heatmap-opacity': {
+  //           default: 1,
+  //           stops: [
+  //             [14, 1],
+  //             [15, 0]
+  //           ]
+  //         }
+  //       }
+  //     },
+  //     'waterway-label'
+  //   );
+
+  //   map.addLayer(
+  //     {
+  //       id: 'trees-point',
+  //       type: 'circle',
+  //       source: 'trees',
+  //       minzoom: 14,
+  //       paint: {
+  //         // increase the radius of the circle as the zoom level and dbh value increases
+  //         'circle-radius': {
+  //           property: 'dbh',
+  //           type: 'exponential',
+  //           stops: [
+  //             [{ zoom: 15, value: 1 }, 5],
+  //             [{ zoom: 15, value: 62 }, 10],
+  //             [{ zoom: 22, value: 1 }, 20],
+  //             [{ zoom: 22, value: 62 }, 50]
+  //           ]
+  //         },
+  //         'circle-color': {
+  //           property: 'dbh',
+  //           type: 'exponential',
+  //           stops: [
+  //             [0, 'rgba(236,222,239,0)'],
+  //             [10, 'rgb(236,222,239)'],
+  //             [20, 'rgb(208,209,230)'],
+  //             [30, 'rgb(166,189,219)'],
+  //             [40, 'rgb(103,169,207)'],
+  //             [50, 'rgb(28,144,153)'],
+  //             [60, 'rgb(1,108,89)']
+  //           ]
+  //         },
+  //         'circle-stroke-color': 'white',
+  //         'circle-stroke-width': 1,
+  //         'circle-opacity': {
+  //           stops: [
+  //             [14, 0],
+  //             [15, 1]
+  //           ]
+  //         }
+  //       }
+  //     },
+  //     'waterway-label'
+  //   );
+  // });
 }
