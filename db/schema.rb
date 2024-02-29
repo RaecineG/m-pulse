a8ff117889430c9cbb5fd76f4364f28c7ed8acdc
Justin Limlengco <jjmlimlengco@gmail.com>
2024-02-29 19:50:06 +0900

    Installed cloudinary

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_24_021724) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_17_035640) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,12 +45,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_24_021724) do
   create_table "badges", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "chatrooms", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
