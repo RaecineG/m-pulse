b1dd0029008038710ae0aea33d18ee46c5e6870b
Megumi Hirao <komegi.blog@gmail.com>
2024-03-07 17:38:41 +0900

    updated error 404 500 page

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,27 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+// const showCircle = (cursor, seconds) => {
+//   // console.log(cursor);
+//   const circle = document.createElement("div");
+//   circle.style.position = "fixed";
+//   circle.style.backgroundColor = "grey";
+//   circle.style.borderRadius = "50%";
+//   circle.style.left = `${cursor.clientX - 16}px`;
+//   circle.style.top = `${cursor.clientY - 16}px`;
+//   circle.style.height = "32px";
+//   circle.style.width = "32px";
+//   circle.style.opacity = 0.8;
+//   document.body.appendChild(circle);
+//   setTimeout(() => {
+//      circle.remove();
+//   }, seconds * 500);
+// };
+
+// document.addEventListener("touchmove", (event) => {
+//   showCircle(event.targetTouches[0], 0.1);
+// });
+// document.addEventListener("click", (event) => {
+//   showCircle(event, 1);
+// });
